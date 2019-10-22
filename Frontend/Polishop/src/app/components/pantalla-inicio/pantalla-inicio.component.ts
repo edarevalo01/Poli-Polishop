700af3c58ed7d8782748f981eba4aa950ed466b3
Alejandro Arevalo <edarevalo01@gmail.com>
2019-10-21 22:34:57 -0500

    Reducción tiempo carga web

--- Frontend/Polishop/src/app/components/pantalla-inicio/pantalla-inicio.component.ts
+++ Frontend/Polishop/src/app/components/pantalla-inicio/pantalla-inicio.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Producto } from 'src/app/model/Producto';
-import { ProductoService } from 'src/app/services/producto.service';
-import { ProductoTemporalService } from 'src/app/services/producto-temporal.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Producto } from "src/app/model/Producto";
+import { ProductoService } from "src/app/services/producto.service";
+import { ProductoTemporalService } from "src/app/services/producto-temporal.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-pantalla-inicio',
-  templateUrl: './pantalla-inicio.component.html',
-  styleUrls: ['./pantalla-inicio.component.css'],
+  selector: "app-pantalla-inicio",
+  templateUrl: "./pantalla-inicio.component.html",
+  styleUrls: ["./pantalla-inicio.component.css"],
   encapsulation: ViewEncapsulation.None
 })
 export class PantallaInicioComponent implements OnInit {
-  title = 'Polishop';
+  title = "Polishop";
   productos: Array<Producto>;
   productosPoli: Array<Producto>;
   productosComu: Array<Producto>;
@@ -20,7 +20,7 @@ export class PantallaInicioComponent implements OnInit {
   matrizPrincipalesComu: Producto[][];
   productoTemp: Producto;
 
-  constructor(private productoService: ProductoService, private productoTemporal: ProductoTemporalService, private router: Router){
+  constructor(private productoService: ProductoService, private productoTemporal: ProductoTemporalService, private router: Router) {
     this.matrizPrincipalesPoli = [];
     this.matrizPrincipalesComu = [];
     this.cargarProductosPoli();
@@ -28,13 +28,13 @@ export class PantallaInicioComponent implements OnInit {
     this.cargarTodosProductos();
   }
 
-  cargarProductosPoli(){
-    this.productoService.getAllProductosByDependencia('Poli').subscribe(
+  cargarProductosPoli() {
+    this.productoService.getAllProductosByDependencia("Poli").subscribe(
       misProductosObs => {
         this.productosPoli = misProductosObs;
       },
       error => {
-        console.error('Error: ', error);
+        console.error("Error: ", error);
       },
       () => {
         console.log("Productos Poli cargados satisfactoriamente.");
@@ -43,14 +43,13 @@ export class PantallaInicioComponent implements OnInit {
     );
   }
 
-  cargarProductosComu(){
-    this.productoService.getAllProductosByDependencia('Comunidad').subscribe(
+  cargarProductosComu() {
+    this.productoService.getAllProductosByDependencia("Comunidad").subscribe(
       misProductosObs => {
-   
         this.productosComu = misProductosObs;
       },
       error => {
-        console.error('Error: ', error);
+        console.error("Error: ", error);
       },
       () => {
         console.log("Productos Comunidad cargados satisfactoriamente.");
@@ -59,7 +58,7 @@ export class PantallaInicioComponent implements OnInit {
     );
   }
 
-  cargarTodosProductos(){
+  cargarTodosProductos() {
     this.productoService.getAllProductos().subscribe(
       misProductosObs => {
         this.productos = misProductosObs;
@@ -73,54 +72,51 @@ export class PantallaInicioComponent implements OnInit {
     );
   }
 
-  cargarProdPeqPoli(){
+  cargarProdPeqPoli() {
     var aux = 0;
-    loop: for(var i = 0; i < 3; i++){
+    loop: for (var i = 0; i < 3; i++) {
       this.matrizPrincipalesPoli[i] = [];
-      for(var j = 0; j < 3; j++){
-        if(aux == 9) break loop;
+      for (var j = 0; j < 3; j++) {
+        if (aux == 9) break loop;
         this.matrizPrincipalesPoli[i][j] = this.productosPoli[aux];
         aux++;
       }
     }
   }
 
-  cargarProdPeqComu(){
+  cargarProdPeqComu() {
     var aux = 0;
-    loop: for(var i = 0; i < 3; i++){
+    loop: for (var i = 0; i < 3; i++) {
       this.matrizPrincipalesComu[i] = [];
-      for(var j = 0; j < 3; j++){
-        if(aux == 9) break loop;
+      for (var j = 0; j < 3; j++) {
+        if (aux == 9) break loop;
         this.matrizPrincipalesComu[i][j] = this.productosComu[aux];
         aux++;
       }
     }
   }
 
-  goProducto(producto){
+  goProducto(producto) {
     this.productoTemp = producto;
     this.productoTemporal.actualProduct = this.productoTemp;
-        
-    this.router.navigate(
-      ['/p-producto/', producto.id],
-      {queryParams: {
-        idProd: this.productoTemp.id, 
+
+    this.router.navigate(["/p-producto/", producto.id], {
+      queryParams: {
+        idProd: this.productoTemp.id,
         nameProd: this.productoTemp.nombre
-      }, 
-      skipLocationChange: false}
-    );
-    this.delay(5);
+      },
+      skipLocationChange: false
+    });
+    this.delay(2);
   }
 
-  doSomething(){
-    console.log('did something');
+  doSomething() {
+    console.log("did something");
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async delay(ms: number) {
-    await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>location.reload());
+    await new Promise(resolve => setTimeout(() => resolve(), ms)).then(() => location.reload());
   }
-
 }
