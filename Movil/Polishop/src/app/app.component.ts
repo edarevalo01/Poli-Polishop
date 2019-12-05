594ae21b3a906108651bb483a9cd8a0de5236781
Alejandro <edarevalo01@gmail.com>
2019-12-05 11:45:36 -0500

    Permisos de cámara (no se sabe que tan estable es)

--- Movil/Polishop/src/app/app.component.ts
+++ Movil/Polishop/src/app/app.component.ts
@@ -4,20 +4,33 @@ import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 
+import { AndroidPermissions } from "@ionic-native/android-permissions/ngx";
+
 @Component({
-  selector: "app-root",
-  templateUrl: "app.component.html",
-  styleUrls: ["app.component.scss"]
+	selector: "app-root",
+	templateUrl: "app.component.html",
+	styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
-    this.initializeApp();
-  }
+	constructor(
+		private platform: Platform,
+		private splashScreen: SplashScreen,
+		private statusBar: StatusBar,
+		private androidPermissions: AndroidPermissions
+	) {
+		this.initializeApp();
+		this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+			(result) => console.log("Has permission?", result.hasPermission),
+			(err) => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
+		);
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleLightContent();
-      this.splashScreen.hide();
-    });
-  }
+		this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA]);
+	}
+
+	initializeApp() {
+		this.platform.ready().then(() => {
+			this.statusBar.styleLightContent();
+			this.splashScreen.hide();
+		});
+	}
 }
--- Movil/Polishop/src/app/app.module.ts
+++ Movil/Polishop/src/app/app.module.ts
@@ -16,24 +16,25 @@ import { ModalPageModule } from "./modal/modal.module";
 import { LinksPageModule } from "./links/links.module";
 import { CompraPageModule } from "./compra/compra.module";
 import { BuscarPageModule } from "./buscar/buscar.module";
-import { PayPal } from '@ionic-native/paypal/ngx';
+import { PayPal } from "@ionic-native/paypal/ngx";
+import { AndroidPermissions } from "@ionic-native/android-permissions/ngx";
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    FormsModule,
-    IonicStorageModule.forRoot(),
-    ModalPageModule,
-    LinksPageModule,
-    CompraPageModule,
-    BuscarPageModule
-  ],
-  providers: [StatusBar, SplashScreen, PayPal, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		FormsModule,
+		IonicStorageModule.forRoot(),
+		ModalPageModule,
+		LinksPageModule,
+		CompraPageModule,
+		BuscarPageModule,
+	],
+	providers: [StatusBar, SplashScreen, PayPal, AndroidPermissions, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
